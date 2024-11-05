3ca87551889f70e81df4efee94140fc04733179a
Takulian <miftakhulghulam10@gmail.com>
2024-11-05 20:11:47 +0700

    add relation in alamat model

--- app/Models/Alamat.php
+++ app/Models/Alamat.php
@@ -2,13 +2,35 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Alamat extends Model
 {
     use HasFactory;
 
-    protected $guarded = ['id'];
     protected $table = 'alamat';
+    protected $primaryKey = 'alamat_id';
+    protected $fillable = [
+        'id_user',
+        'no_telepon',
+        'label_alamat',
+        'nama_penerima',
+        'detail',
+        'kelurahan',
+        'kecamatan',
+        'kabupaten',
+        'provinsi',
+        'kodepos',
+        'isPrimary',
+        'catatan_kurir'
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'id_user', 'user_id');
+    }
+
 }
