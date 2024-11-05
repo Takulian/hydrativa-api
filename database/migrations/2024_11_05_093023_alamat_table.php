dbd9f6e81c4c165550447298c1f3ebf8a92b4894
Takulian <miftakhulghulam10@gmail.com>
2024-11-05 09:47:30 +0700

    add alamat table

---
+++ database/migrations/2024_11_05_093023_alamat_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('alamat', function (Blueprint $table) {
+            $table->id('alamat_id');
+            $table->string('label_alamat');
+            $table->string('nama_penerima');
+            $table->string('alamat');
+            $table->string('isMain');
+            $table->string('catatan_kurir')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
