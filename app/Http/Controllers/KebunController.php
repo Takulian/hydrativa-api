8f21b742cb5add4713aeea9ced585d79cadf675f
Takulian <miftakhulghulam10@gmail.com>
2024-11-06 08:43:41 +0700

    forgot to impor package

--- app/Http/Controllers/KebunController.php
+++ app/Http/Controllers/KebunController.php
@@ -8,6 +8,7 @@ use App\Models\Kebun;
 use App\Models\Histori;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\File;
 use App\Http\Resources\KebunResource;
 use App\Http\Resources\HistoriResource;
 
