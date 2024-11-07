00b66b96cdc8faaea4c2f8c78e55c58eef6d061a
Takulian <miftakhulghulam10@gmail.com>
2024-11-07 14:34:49 +0700

    new return detail style

--- app/Http/Resources/ProdukResource.php
+++ app/Http/Resources/ProdukResource.php
@@ -14,6 +14,10 @@ class ProdukResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        $rating = $this->transaksiItem->map(function ($item) {
+            return $item->rating ? $item->rating->rating : null;
+        })->filter()->values()->all();
+
         return [
             'id' => $this->produk_id,
             'nama' => $this->nama_produk,
@@ -22,8 +26,16 @@ class ProdukResource extends JsonResource
             'harga' => $this->harga,
             'gambar' => 'http://127.0.0.1:8000/storage/' . $this->gambar,
             'stok' => $this->stok,
+            'final_rating' => round(array_sum($rating)/count($rating),1),
             'rating' => $this->transaksiItem->map(function ($item) {
-                return $item->rating;
+                return $item->rating ? [
+                    'nama_user' => $item->rating->user->name,
+                    'rating_user' => $item->rating->rating,
+                    'komen_user' => $item->rating->comment,
+                    'gambar_komen' => $item->rating->gambar,
+                    'tanggal' => date_format($item->rating->created_at, "Y/m/d H:i:s")
+
+                ] : null;
             })->filter()->values()->all()
         ];
     }
