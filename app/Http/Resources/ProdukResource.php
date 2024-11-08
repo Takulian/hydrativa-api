3f303e8a8af1c757ea2993101bc84fdb395f729a
tgrsnts <tegarsantoso72@gmail.com>
2024-11-08 11:18:34 +0700

    nambahin response detail

--- app/Http/Resources/ProdukResource.php
+++ app/Http/Resources/ProdukResource.php
@@ -33,6 +33,35 @@ class ProdukResource extends JsonResource
             ] : null;
         })->filter()->values()->all();
 
+        // Calculate total number of ratings
+        $jumlahRating = count($rating);
+
+        // Count ratings with comments
+        $jumlahRatingDenganKomen = $this->transaksiItem->filter(function ($item) {
+            return $item->rating && !empty($item->rating->comment);
+        })->count();
+
+        // Calculate the number of users who rated 1, 2, 3, 4, 5 and their percentages
+        $ratingCounts = array_count_values($rating);
+        $ratingDistribution = [];
+        for ($i = 1; $i <= 5; $i++) {
+            $count = $ratingCounts[$i] ?? 0;
+            $percentage = $jumlahRating > 0 ? round(($count / $jumlahRating) * 100, 2) : 0;
+            $ratingDistribution[$i] = [
+                'jumlah' => $count,
+                'persentase' => $percentage
+            ];
+        }
+
+        // Calculate the percentage of satisfied customers (rating 4 or 5)
+        $jumlahPuas = ($ratingCounts[4] ?? 0) + ($ratingCounts[5] ?? 0);
+        $persentasePuas = $jumlahRating > 0 ? round(($jumlahPuas / $jumlahRating) * 100, 2) : 0;
+
+        // Get the total number of items sold
+        $jumlahTerjual = $this->transaksiItem->filter(function ($item) {
+            return $item->transaksi && $item->transaksi->status === "success";
+        })->count();
+        
         return [
             'id' => $this->produk_id,
             'nama' => $this->nama_produk,
@@ -42,7 +71,12 @@ class ProdukResource extends JsonResource
             'gambar' => 'http://127.0.0.1:8000/storage/' . $this->gambar,
             'stok' => $this->stok,
             'final_rating' => $finalRating, // The calculated final rating
-            'rating' => $ratings // The individual ratings for each user
+            'jumlah_rating' => $jumlahRating, // Total number of ratings
+            'jumlah_ulasan' => $jumlahRatingDenganKomen, // Number of ratings with comments
+            'rating' => $ratings, // The individual ratings for each user
+            'distribusi_rating' => $ratingDistribution, // Distribution of ratings and percentages
+            'persentase_puas' => $persentasePuas, // Percentage of satisfied customers
+            'jumlah_terjual' => $jumlahTerjual // Total number of items sold
         ];
     }
 }
